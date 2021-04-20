4691cd3745e455cfbd6afff4361c114f2238b04b
Phinner <62483793+Phinner@users.noreply.github.com>
2021-04-20 18:43:59 +0200

    init

---
+++ src/main/java/fr/xpdustry/plugins/template/TemplateClass.java
@@ -0,0 +1,26 @@
+package fr.xpdustry.plugins.template;
+
+import arc.util.*;
+
+import mindustry.gen.*;
+import mindustry.mod.Plugin;
+
+public class TemplateClass extends Plugin{
+
+    @Override
+    public void init(){
+        //called when game initializes
+    }
+
+
+    @Override
+    public void registerServerCommands(CommandHandler handler){
+        //register commands that run on the server
+    }
+
+
+    @Override
+    public void registerClientCommands(CommandHandler handler){
+        //register commands that player can invoke in-game
+    }
+}
